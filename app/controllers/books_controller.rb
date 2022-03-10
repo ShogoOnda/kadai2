653918946e29cf8e74e850c4eb6cfbfce0e29502
EC2 Default User <ec2-user@ip-172-31-8-245.ec2.internal>
2022-03-10 16:09:33 +0000

    [add] バリデーションエラーメッセージ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,16 +5,17 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-
+    @books = Book.all
     if @book.save
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: "Book was successfully created."
     else
-      render :new
+      render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -28,7 +29,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id), notice: "Book was successfully updated."
   end
 
   def destroy
